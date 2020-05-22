8574a97c9ffcd2064cd10b6e6d8040650792f9c4
Barry Liu <liubaolin20070125@outlook.com>
2020-05-22 21:39:41 +0800

    UpDate！！！！！！！

--- script/Control.py
+++ script/Control.py
@@ -1,7 +1,7 @@
 import colorfulText
 import Keys
 import Error
-import time
+import time,sys
 def Hi():
     colorfulText.printRed("""
     ###   ###  #########  #########
@@ -11,12 +11,40 @@ def Hi():
     ###   ###     ###       #####
     ###   ###  #########    #####""")
 def Intro():
-    colorfulText.printWhiteBlack("\nThanks for you choose Natural-Disasters.")
-    colorfulText.printWhiteBlack("\nThis is a Menu.")
-    colorfulText.printWhiteBlack("\nWe are Bug-Maker Project.")
+    colorfulText.printWhiteBlack("\n Thanks for you choose Natural-Disasters.")
+    colorfulText.printWhiteBlack("\n This is a Menu.")
+    colorfulText.printWhiteBlack("\n We are Bug-Maker Project.")
+def Menu():
+    colorfulText.printGreen("""
+    ###################################
+    #1.激活码输入                     #
+    #2.作弊码键入                     #
+    #3.Devlop Mode                    #
+    #4.Exit                           #
+    ###################################
+    """)
+    reponse=input("\n\n\n>>>")
+    if reponse=="1":
+        Key=input("Enter:")
+        isKeyCorrect=Keys.isIn(Key,Keys.read(Keys.returnObject("..//Data//Keys.xls")))
+        if isKeyCorrect:
+            with open("..//Data//Keys.Key","wb") as j:
+                j.write(bytes(Key,"utf-8"))
+            colorfulText.printGreen("OK\n")
+        else:
+            colorfulText.printGreen("Incorrect!\n")
+    elif reponse=="2":
+        pass
+    elif reponse=="3":
+        pass
+    elif reponse=="4":
+        sys.exit()
 def Main():
     Hi()
     time.sleep(1)
     Intro()
     time.sleep(3)
-Main()
+    while True:
+        Menu()
+if __name__=="__main__":
+    Main()
