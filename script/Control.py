6d52b1515d54483b11a44933d29e91418340c5c5
Barry Liu <liubaolin20070125@outlook.com>
2020-05-22 21:17:03 +0800

    Control

--- script/Control.py
+++ script/Control.py
@@ -10,7 +10,13 @@ def Hi():
     #########     ###
     ###   ###     ###       #####
     ###   ###  #########    #####""")
+def Intro():
+    colorfulText.printWhiteBlack("\nThanks for you choose Natural-Disasters.")
+    colorfulText.printWhiteBlack("\nThis is a Menu.")
+    colorfulText.printWhiteBlack("\nWe are Bug-Maker Project.")
 def Main():
     Hi()
+    time.sleep(1)
+    Intro()
     time.sleep(3)
 Main()
--- script/__init__.py
+++ script/__init__.py
@@ -1,1 +1,1 @@
-__all__=["maths","item","sprite"]
+
