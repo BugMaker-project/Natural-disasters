afdaf025e4805848da08ae20599d0a2c9c987a18
lzhbhlrPython <Liubaolin20070125@outlook.com>
2020-10-28 17:16:58 +0800

    devin v0.1.0

--- Engine/Until/__init__.py
+++
@@ -1,2 +0,0 @@
-import Engine.Until.canvas_until as canvas_until
-import Engine.Until.object_init as object_init
--- Engine/Until/object_init.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.font.init()
-def FontLoad(path:str,size:int):
-    tempFont=pygame.font.Font(path,size)
-    return tempFont
